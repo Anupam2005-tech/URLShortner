66c5b4eff1e984a6d7e74b730c10c97a40f72b7b
Anupam2005-tech <abhowmik068@gmail.com>
2025-05-31 18:31:30 +0530

    minor fix and bugs fixed

--- frontend/URLshortner/src/App.tsx
+++ frontend/URLshortner/src/App.tsx
@@ -1,14 +1,13 @@
 import URLpage from "./components/URLpage/URLpage";
 import RegistrationForm from "./components/authentication/register";
 import LoginForm from "./components/authentication/login";
-import { Routes, Route, Navigate } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import ProtectedRoute from "../protectedRoutes/protectedRoute";
+import { Routes, Route } from "react-router-dom";
+
 import Dashboard from "./components/dashboard/dashboard";
 import URLanalytics from "./components/URLpage/URLanalytics";
 
 function App() {
-  // const { isAuthenticated } = useSelector((state: any) => state.root);
+  
 
   return (
     <>
@@ -16,13 +15,13 @@ function App() {
         <Route path="/" element={<Dashboard/>} />
         <Route path="/user/login" element={<LoginForm />} />
         <Route path="/register" element={<RegistrationForm />} />
-        {/* protected route */}
+
         <Route
           path="/url"
           element={
-            // <ProtectedRoute isAuthenticated={isAuthenticated}>
+
               <URLpage />
-            /* </ProtectedRoute> */
+
           }
         />
         <Route path="/url/analytics" element={<URLanalytics/>} />
--- frontend/URLshortner/src/main.tsx
+++ frontend/URLshortner/src/main.tsx
@@ -2,13 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { createRoot } from "react-dom/client";
-import { store } from "../redux/store.ts";
-import { Provider } from "react-redux";
+
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
+
       <App />
-    {/* </Provider> */}
+
   </BrowserRouter>
 );
