6042b2f835b5aa54bda3c44d833f7146f4aade6d
Anupam2005-tech <abhowmik068@gmail.com>
2025-06-04 10:58:42 +0530

    render deploy

--- backend/MongoDB.js
+++ backend/MongoDB.js
@@ -4,8 +4,6 @@ mongoose.set('strictQuery', true);
 
 async function mongoDBconnect(url) {
   return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     connectTimeoutMS: 120000, 
   });
 }
